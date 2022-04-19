25f4473e12ec7484b212cd1abbb0b3dd7d28f510
Flashrob <kolsek.income@gmail.com>
2022-04-19 17:46:24 +0800

    feature: Delete one todo item

--- src/App.js
+++ src/App.js
@@ -9,13 +9,21 @@ function App() {
     setInput('');
   };
 
+  const deleteTodoHandler = (indexToDelete) => {
+    const todosAfterDelete = todos.filter((_todo, todoIndex) => todoIndex !== indexToDelete)
+    setTodos(todosAfterDelete);
+  };
+
   return (
     <>
       <h1>Todo Application</h1>
       <input value={input} onChange={(e) => setInput(e.target.value)} />
       <button onClick={createTodoHandler}>Create Todo</button>
       {todos.map((todo, index) => (
-        <p key={todo + index}>{todo}</p>
+        <div>
+          <span key={todo + index}>{todo}</span>
+          <button onClick={() => deleteTodoHandler(index)}>Delete</button>
+        </div>
       ))}
     </>
   );
