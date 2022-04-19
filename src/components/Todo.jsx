96127b65655cd250a78d8b4ab68e5997863f7070
Flashrob <kolsek.income@gmail.com>
2022-04-19 18:25:53 +0800

    feature: Mark todo as done

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,12 @@
+import { useState } from 'react';
+import './Todo.css';
+
 export const Todo = ({ todo, index, deleteTodo }) => {
+    const [isDone, setIsDone] = useState(false);
+
     return (
     <div>
-        <span key={todo + index}>{todo}</span>
+        <span {...(isDone && { className: 'done' })} key={todo + index} onClick={() => setIsDone(!isDone)}>{todo}</span>
         <button onClick={() => deleteTodo(index)}>Delete</button>
     </div>
     )
