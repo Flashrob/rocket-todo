6943f66333e6f3302381120b08854b363e7445d4
Flashrob <kolsek.income@gmail.com>
2022-04-19 16:27:56 +0800

    feature: Create controlled input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { useState } from 'react';
+
 function App() {
-  return <h1>Todo Application</h1>;
+  const [input, setInput] = useState('');
+  return (
+    <>
+      <h1>Todo Application</h1>
+      <input value={input} onChange={(e) => setInput(e.target.value)} />
+      <button>Create Todo</button>
+    </>
+  );
 }
 
 export default App;
